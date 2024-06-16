d102a35b2302be0d63d0bcf51e27abf9eee8d4f7
stiven de la rosa brito <codestiven@gmail.com>
2024-06-16 18:12:28 -0400

    03.Routing en Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludo', function () {
+    return view('nuevavista');
+});
+
+
+Route::get('/saludo/{name}', function ($name) {
+    return "Hola , $name";
+});
+
+//Route::view('/', 'welcome' , ['mensaje' => 'saludo']);
